aeb826fbdb6d2a7ac59a47f951097ee3886e17c6
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-05-29 22:56:36 +0200

    Created a custom directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
+import {BasicHighlightDirective} from "./basicHighlight/basicHighlight.directive";
 import { AppComponent } from './app.component';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
