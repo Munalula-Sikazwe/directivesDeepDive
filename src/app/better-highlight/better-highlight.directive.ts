4d96543f1bee33fbc65c6f22f0f6541c01733cc4
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-05-29 23:59:40 +0200

    Used a host listener to make the directive effects more dynamic .

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, Renderer2, OnInit, ElementRef} from '@angular/core';
+import {Directive, Renderer2, OnInit, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -7,7 +7,12 @@ export class BetterHighlightDirective implements OnInit{
 
   constructor( private elref:ElementRef, private renderer:Renderer2) { }
   ngOnInit() {
-    this.renderer.setStyle(this.elref.nativeElement,'background-color','red')
-  }
 
+  }
+@HostListener('mouseenter') mousein(event: Event){
+     this.renderer.setStyle(this.elref.nativeElement,'background-color','red')
+}
+@HostListener('mouseleave') mouseout(event: Event){
+     this.renderer.setStyle(this.elref.nativeElement,'background-color','transparent')
+}
 }
